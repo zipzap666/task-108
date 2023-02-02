708158d698f4335bdfd79026159599b3e1f0338c
zipzap666 <zipzap666@mail.ru>
2023-02-02 16:18:03 +0300

    upd parserTools

--- task_solution/parser/parserTools.py
+++ task_solution/parser/parserTools.py
@@ -1,52 +1,51 @@
-from google.protobuf.internal import decoder
-from copy import *
-from proto.message_pb2 import *
+from typing import TypeVar, Type
+from google.protobuf.internal.decoder import _DecodeVarint32
+from google.protobuf.message import DecodeError
 
 
-def parse_delimited(data, typename):
+def parse_delimited(data, message_type):
     if data is None or len(data) == 0:
-        return None
+        return None, 0
 
-    length, pos = decoder._DecodeVarint32(data, 0)
+    try:
+        length, pos = _DecodeVarint32(data, 0)
+    except TypeError:
+        return None, 0
 
     if length + pos > len(data):
         return None, 0
 
-    message = typename
-    message.ParseFromString(data[pos:pos+length])
+    message = message_type()
+    try:
+        message.ParseFromString(data[pos:pos + length])
+    except DecodeError:
+        return None, pos + length
 
-    return message, length + pos
+    return message, pos + length
+
+
+T = TypeVar('T')
 
 
 class DelimitedMessagesStreamParser:
-    def __init__(self, type_msg) -> None:
-        self.buffer = b''
+    def __init__(self, type_msg: Type[T]):
         self.type_msg = type_msg
+        self.buffer: bytearray = b''
 
-    def parse(self, data,):
-        self.buffer += data
-        byte_consumed = 0
-        messages = []
+    def parse(self, data) -> list[Type[T]]:
+        try:
+            self.buffer += data
+        except TypeError:
+            return []
 
-        while True:
-            prev = byte_consumed
-            message, byte_consumed = parse_delimited(self.buffer[byte_consumed:], copy(self.type_msg))
-            print(byte_consumed)
-
-            if message is not None:
+        messages: list[Type[T]] = []
+        while len(self.buffer):
+            message, bytes_consumed = parse_delimited(self.buffer, self.type_msg)
+            if message:
                 messages.append(message)
-            elif byte_consumed == prev:
+            elif bytes_consumed == 0:
                 break
 
-            if byte_consumed == len(self.buffer):
-                break
-
-        self.buffer = self.buffer[byte_consumed:]
+            self.buffer = self.buffer[bytes_consumed:]
 
         return messages
-
-
-if __name__ == '__main__':
-    # message = WrapperMessage()
-    parser = DelimitedMessagesStreamParser(WrapperMessage())
-    lst = parser.parse(b"\x05\n\x03\n\x01\x01\x05\n\x03\n\x01\x01")
